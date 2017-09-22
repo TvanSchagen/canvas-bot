e462cccedf2bacccb4b187ec80970c82815885da
Ivo de Geus <mail4ivo@gmail.com>
2017-09-22 15:58:09 +0200

    'wew'

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const TelegramBot = require('node-telegram-bot-api');
+http = require('http');
+finalhandler = require('finalhandler');
+serveStatic = require('serve-static');
+randomInt = require('random-int');
 
 const canvasToken = '367658684:AAEG7HFK_raGDEkrIKNln5OrxDeulzUqGPs';
 const canvasBot = new TelegramBot(canvasToken, {polling: true});
+
+// Static file serving
+var serve = serveStatic("./");
+var server = http.createServer(function(req, res) {
+    var done = finalhandler(req, res);
+    serve(req, res, done);
+});
+server.listen(80);
+
+
+
+// Bot
 canvasBot.on('text', function(msg){
 
 	console.log(msg);
     if (msg.text.toLowerCase().includes("willem")) {
         canvasBot.sendMessage(msg.chat.id, "Hoorde ik onze koning?");
-		canvasBot.sendAudio(msg.hatId, './stillewillem.mp3');
+
+        if(randomInt(1,2) === 1){
+            canvasBot.sendAudio(msg.chat, 'https://canvasstudiobot.herokuapp.com/stillewillem.mp3');
+        }else{
+            canvasBot.sendAudio(msg.chat, 'https://canvasstudiobot.herokuapp.com/specerijen.mp3');
+        }
     }
 
     if (msg.text.toLowerCase().includes("nijs")) {
@@ -15,14 +36,3 @@ canvasBot.on('text', function(msg){
     }
 
 });
-
-canvasBot.on('text', function(msg){
-
-	console.log(msg);
-    if (msg.text.toLowerCase().includes("willem")) {
-        canvasBot.sendMessage(msg.chat.id, "Hoorde ik onze koning?");
-		canvasBot.sendAudio(msg.hatId, 'https://canvasstudiobot.herokuapp.com/stillewillem.mp3');
-    }
-
-});
-
