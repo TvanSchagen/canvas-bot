74b346fdf3e9262464b1d487c959c82cbf82e758
TvanSchagen <551120@student.inholland.nl>
2017-09-23 21:02:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ server.listen(process.env.PORT || 80);
 // Bot procedures
 canvasBot.on('text', function(msg){
 
-    console.log(msg);
-
     // Willem
     if (msg.text.toLowerCase().includes("willem")) {
         canvasBot.sendMessage(msg.chat.id, "Hoorde ik onze koning?");
@@ -101,19 +99,10 @@ canvasBot.on('text', function(msg){
     if (msg.text.toLowerCase().includes("canvas")) {
         canvasBot.sendMessage(msg.chat.id, 'Ja?');
     }
-
-    // Canvas
-    if (msg.text.toLowerCase().includes("0")) {
-        canvasBot.sendMessage(msg.chat.id, msg.text.replace(/e/g, '0').replace(/o/g, '0'));
-    }
 	    
     // Nicki
     if (msg.text.toLowerCase().includes("nicki")) {
-        if(randomInt(1,3) === 1){
-            canvasBot.sendMessage(msg.chat.id, "`.nicki-minaj { ass-size: cover; }`", {"parse_mode":"Markdown"});
-        }else{
-            canvasBot.sendMessage(msg.chat.id, "_Mijn anaconda niet wil niets als je geen bolletjes hebt, honing!_",{"parse_mode":"Markdown"});
-        }
+        canvasBot.sendVoice(msg.chat.id, 'http://canvasstudio-telegrambot.herokuapp.com/assets/biem.mp3');
     }
 
     // Moe
@@ -130,5 +119,23 @@ canvasBot.on('text', function(msg){
     if (msg.text.toLowerCase().includes("graag gedaan")) {
         canvasBot.sendMessage(msg.chat.id, "Super de Boer!");
     }
+	
+    // GameMaker
+    if (msg.text.toLowerCase().includes("gamemaker")) {
+	var random = randomInt(1,4)
+        if(random == 1) {
+		canvasBot.sendMessage(msg.chat.id, "GameMaker is de epitoom der spelmaakprogramma's op de markt in dit huidige jaar, en ja, ik durf zelfs te zeggen, in de komende 100 jaar.");
+	} else if (random == 2) {
+		canvasBot.sendMessage(msg.chat.id, "GameMaker overkomt raciale barrières en gender. Het is een schande dat dit wonderprogramma nog nooit nobelprijs voor de vrede heeft gewonnen.");
+	} else if (random == 3) {
+		canvasBot.sendMessage(msg.chat.id, "Mijn opa game-makete zijn hele leven. Ik was 10 jaar toen mijn moeder tegen hem zei: 'als je ooit je kleinkinderen wilt zien afstuderen, moet je onmiddellijk stoppen.' Tranen rolde over zijn wangen toen hij doorkreeg wat er precies speelde. Hij stopte meteen. Drie jaar later stierf hij aan RSI. Het was heel verdrietig en sloopte mij. Mijn moeder zei tegen me: 'Ga nooit game-maken. Laat alsjeblieft je familie nooit mee maken wat opa ons heeft aangedaan.' Ik ging akkoord. Ik ben nu 28 en heb nog nooit GameMaker aangeraakt. Ik moet zeggen, ik voel wat spijt dat ik het nooit gedaan heb, want jouw bericht heeft mij overtuigd.");
+	} else if (random == 4) {
+		canvasBot.sendMessage(msg.chat.id, "GameMaker is de god op deze aarde. ");
+	}
+    }
+	
+    if (msg.text.toLowerCase().includes("vuurwerk")) {
+        canvasBot.sendMessage(msg.chat.id, "Wat de fuck zei je over mij, brutaaltje?? dat je het weet, ik kan het snelste twintig Cobra 6en door je brievenbus gooien van al mijn maten, ik heb meegewerkt aan kEIHARDE knallen, de vernietiging van meerdere prullenbakken en asociaal gedrag vertoond met mijn vuurwerk tegen meer dan duizend mensen. Jij bent slechts een flauw rotje voor mij. Ik ben getraind om alles helemaal naar de tyfus te knallen binnen een flits. Taskforce OVB heeft geen grip op mij. Let op mijn woorden, als je nog één keer die protestbek van je opentrekt dauw ik hoogstpersoonlijk tien lawinepijlen in je reet. Denk je dat je zo tegen mij kan praten omdat we op het internet zitten? Denk nog eens na, flauw flikkersterretje. Terwijl we praten heb ik mijn geheime netwerk van illegale handelaren en -vuurwerkafstekers ingezet en wordt je huis getraceerd dus bereid je maar vast voor op de tientallen vuurwerkbommen die door je brievenbus zullen worden gejast, brillenbeschermerdragertje. De vuurwerkbommen die je eens een lesje zullen leren, zielige Romeinse kaars die je bent! Je gaat er aan, cultuurbarbaar. Ik en mijn maten kunnen overal tegelijk zijn en op zevenhonderd verschillende manieren je laten schrikken door vuurwerk, en dat is alleen nog maar om jou zielige anti-vuurwerkreet bang te maken. Ik ben niet alleen in staat om je zielige huisje te bombarden met dikke fireworks maar ik heb de beschikking over de gehele illegale vuurwerkcollectie uit het Oostblok en ik zal er voor zorgen dat jij de komende honderd jaar tijdens Oud&Nieuw steevast binnen blijft. Als je zou weten wat voor domme gevolgen je 'slimme' comment over dat ‘vuurwerk verboden zou moeten worden' over je af heeft geroepen, had je misschien die brutale bek gehouden. Maar dat deed je niet, en nu betaal je de uiterste prijs, gedoofd lontje dat je bent. PYROMANIACS WILL NEVER DIE!!!");
+    }	
 
 });
