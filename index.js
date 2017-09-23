84cec1703e509a29eda8f8768112ba882f48f3a8
Jonah Meijers <jonahmeijers97@gmail.com>
2017-09-23 20:39:26 +0200

    Werkt dit dan wel?

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ canvasBot.on('text', function(msg){
     // Nicki
     if (msg.text.toLowerCase().includes("moe")) {
         canvasBot.sendMessage(msg.chat.id, "Ik ben moe en ga slapen, doei");
-        canvasBot.setTimeout(function() {
+        document.setTimeout(function() {
             canvasBot.sendMessage(msg.chat.id, "Zo, genoeg gelachen... Maar was wel lachen he");
         }, 2000);
     }
